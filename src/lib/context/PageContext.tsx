bf321e4006b366a314320682b221a0eeb07cf397
wonieeVicky <hwfongfing@gmail.com>
2024-05-22 22:35:57 +0900

    feat: PageContext, useSessionStorage 정의

---
+++ src/lib/context/PageContext.tsx
@@ -0,0 +1,36 @@
+import { Dispatch, ReactNode, createContext, useContext } from 'react';
+import useSessionStorage from '../hooks/useSessionStorage';
+
+export type PageDispatch = Dispatch<any>;
+
+interface Props {
+  children: ReactNode;
+}
+
+const PageStateContext = createContext<number | undefined>(undefined);
+
+const PageDispatchContext = createContext<PageDispatch | undefined>(undefined);
+
+const PageContextProvider = ({ children }: Props) => {
+  const [page, setPage] = useSessionStorage('offset', 0);
+
+  return (
+    <PageDispatchContext.Provider value={setPage}>
+      <PageStateContext.Provider value={page}>
+        {children}
+      </PageStateContext.Provider>
+    </PageDispatchContext.Provider>
+  );
+};
+
+export const usePageState = () => {
+  const state = useContext(PageStateContext);
+  return state;
+};
+
+export const usePageDispatch = () => {
+  const dispatch = useContext(PageDispatchContext);
+  return dispatch;
+};
+
+export default PageContextProvider;
---
+++ src/lib/hooks/useSessionStorage.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const useSessionStorage = (key: string, initialValue: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const item = window.sessionStorage.getItem(key);
+      return !!item ? JSON.parse(item) : initialValue;
+    } else {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: any) => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+    setStoredValue(valueToStore);
+    if (typeof window !== 'undefined') {
+      window.sessionStorage.setItem(key, JSON.stringify(valueToStore));
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useSessionStorage;
