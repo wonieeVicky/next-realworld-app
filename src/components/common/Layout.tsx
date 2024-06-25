cf61922a32c998045c6094bb6117ca18b4a6cb83
wonieeVicky <hwfongfing@gmail.com>
2024-06-25 23:26:19 +0900

    feat: Layout 내 Footer 추가

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 type Props = {
   children: ReactNode;
@@ -9,6 +10,7 @@ const Layout = ({ children }: Props) => (
   <>
     <Navbar />
     {children}
+    <Footer />
   </>
 );
 
