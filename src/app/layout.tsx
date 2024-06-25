071d66f7442cf06bcbfbb927e8b75dd2294c294b
wonieeVicky <hwfongfing@gmail.com>
2024-06-25 23:26:06 +0900

    feat: layout 내 미사용 import 제거, font cdn 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,6 @@ import Layout from '@/components/common/Layout';
 import Head from 'next/head';
 import localFont from 'next/font/local';
 
-import 'https://demo.productionready.io/main.css';
-
 export const metadata: Metadata = {
   title: {
     default: 'Home',
@@ -38,13 +36,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${pretendard.variable}`}>
       <body className={pretendard.className}>
-        <link
-          rel="stylesheet"
-          href="https://demo.productionready.io/main.css"
-        />
+        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
         <link
           rel="stylesheet"
           href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic&display=swap"
         />
         <ContextProvider>
           <Layout>{children}</Layout>
