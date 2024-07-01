55c32bd5a4c7783666eaabdf45b74fee228d7e56
wonieeVicky <hwfongfing@gmail.com>
2024-07-01 23:43:02 +0900

    feat: MainView 내 ArticleList 추가

--- src/components/main/MainView.tsx
+++ src/components/main/MainView.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import TabList from './TabList';
-// import ArticleList from "../article/ArticleList";
+import ArticleList from '../article/ArticleList';
 
 const MainView = () => (
   <div className="col-md-9">
     <div className="feed-toggle">
       <TabList />
     </div>
-    {/* <ArticleList /> */}
+    <ArticleList />
   </div>
 );
 
