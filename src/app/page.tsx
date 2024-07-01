efd897b3c83cb1a7e4e5c45c9961088b779c8382
wonieeVicky <hwfongfing@gmail.com>
2024-07-01 23:47:16 +0900

    feat: Main 내 Tags 영역 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from '@/components/main/Banneer';
 import MainView from '@/components/main/MainView';
+import Tags from '@/components/main/Tags';
 
 export default function Home() {
   return (
@@ -8,6 +9,12 @@ export default function Home() {
       <div className="container page">
         <div className="row">
           <MainView />
+          <div className="col-md-3">
+            <div className="sidebar">
+              <p>Popular Tags</p>
+              <Tags />
+            </div>
+          </div>
         </div>
       </div>
     </div>
